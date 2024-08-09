1df684c5159fc505d50414893433e84c564c58a1
Klofrox <klofrox7@gmail.com>
2024-08-09 14:12:04 +0300

    reactDelay

--- public/electron.js
+++ public/electron.js
@@ -61,27 +61,30 @@ appPos.post('/print-receipt', (req, res) => {
 });
 
 function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true,
-      enableRemoteModule: false,
-      nodeIntegration: false
-    }
-  });
-
+    const mainWindow = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+        contextIsolation: true,
+        enableRemoteModule: false,
+        nodeIntegration: false
+      },
+      autoHideMenuBar: true, // Menü çubuğunu gizler
+    });
   mainWindow.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
-  createWindow();
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    setTimeout(() => {
+      createWindow();
+    }, 8000); // 10 saniye bekleme süresi (10,000 milisaniye)
+  
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
   });
-});
-
+  
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
